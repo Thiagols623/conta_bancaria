34ce4a6eea22d68d296161f9648a01a5f5128b1a
Thiago Lima <thiagolima6623@gmail.com>
2026-05-05 18:30:01 -0300

    Implementando a Classe Conta como Classe Abstrata

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -9,15 +9,9 @@ import conta_bancaria.util.Cores;
 
 public class Menu {
 
-	public static void main(String[] args) {
+	private static final Scanner leia = new Scanner(System.in);
 
-		// Teste da Classe Conta
-		Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
+	public static void main(String[] args) {
 
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
@@ -34,8 +28,6 @@ public class Menu {
 		cp1.visualizar();
 		cp1.depositar(5000.0f);
 		cp1.visualizar();
-
-		Scanner leia = new Scanner(System.in);
 
 		int opcao;
 
