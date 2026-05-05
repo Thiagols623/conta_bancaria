5dc522280485503e92f36d020ef03874fb84746d
Thiago Lima <thiagolima6623@gmail.com>
2026-05-04 21:57:23 -0300

    Classe Model Conta

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -2,10 +2,26 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
+import conta_bancaria.model.Conta;
 import conta_bancaria.util.Cores;
 
 public class Menu {
 	public static void main(String[] args) {
+
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
+
+		c1.visualizar();
+
+		c1.setSaldo(15000.0f);
+		c1.setTitular("Maria Joaquina");
+		c1.visualizar();
+
+		c1.sacar(12000.0f);
+		c1.visualizar();
+
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
@@ -81,14 +97,13 @@ public class Menu {
 				break;
 			}
 		}
-
 	}
 
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
-		System.out.println("Projeto Desenvolvido por: ");
-		System.out.println("Generation Brasil - generation@generation.org");
-		System.out.println("github.com/conteudoGeneration");
+		System.out.println("Projeto Desenvolvido por: Thiago Lima Santos ");
+		System.out.println("Generation Brasil - thiagos@genstudents.org");
+		System.out.println("https://github.com/Thiagols623");
 		System.out.println("*********************************************************");
 	}
 
